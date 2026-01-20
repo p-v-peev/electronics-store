47b9d237abcf0fbc1cd25a05c7bf4b64d36cd5a4
Plamen Peev <p.v.peev@gmail.com>
2026-01-20 21:20:41 +0200

    Init repository

---
+++ src/main/java/com/example/pvpeev/electronics_store/ElectronicsStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.pvpeev.electronics_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsStoreApplication.class, args);
+	}
+
+}
