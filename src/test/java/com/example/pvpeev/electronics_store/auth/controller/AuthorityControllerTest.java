d9f94473453bf26f34880bcf45ca64137fcbb25e
Plamen Peev <p.v.peev@gmail.com>
2026-02-02 12:29:26 +0200

    Add AuthorityController integration test

---
+++ src/test/java/com/example/pvpeev/electronics_store/auth/controller/AuthorityControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.pvpeev.electronics_store.auth.controller;
+
+import com.example.pvpeev.electronics_store.auth.dto.AuthorityResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.list;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthorityControllerTest {
+
+    @Autowired
+    MockMvcTester mockMvcTester;
+
+    @Test
+    public void testGetAllAuthorities() {
+        assertThat(mockMvcTester.get().uri(AuthorityController.PATH))
+                .hasStatusOk()
+                .bodyJson()
+                .convertTo(list(AuthorityResponse.class))
+                .extracting(AuthorityResponse::getName)
+                .containsExactlyInAnyOrder(
+                        "ROLE_STORE_USER",
+                        "ROLE_STORE_WAREHOUSE_WORKER",
+                        "ROLE_STORE_PRODUCT_ADMIN",
+                        "ROLE_STORE_AUTHORITY_ADMIN"
+                );
+    }
+}
